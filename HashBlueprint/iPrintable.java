117f2080c7a960f0e8e2f2cf08d70b3f90f3f61f
hmezer <147049283+hmezer@users.noreply.github.com>
2024-02-29 15:43:52 +0300

    Create iPrintable.java

---
+++ HashBlueprint/iPrintable.java
@@ -0,0 +1,13 @@
+package HashBlueprint;
+
+/*
+ * Created just to be able to use the same code for printing multiple data structures, nothing of importance
+ */
+public interface iPrintable<Key> {
+  public Iterable<Key> keySet();
+  
+  public Object get(Key key);
+  
+  public int size();
+
+}
